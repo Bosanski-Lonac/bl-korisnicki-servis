03babe3e1b15060fd3d91beffc28ddbed5d4747a
Andrej Gasic <andrej@gasic.rs>
2020-12-15 21:19:14 +0100

    Uklonjen whitespace

--- src/main/java/com/bosanskilonac/ks/KorisnickiServisRun.java
+++ src/main/java/com/bosanskilonac/ks/KorisnickiServisRun.java
@@ -1,6 +1,5 @@
 package com.bosanskilonac.ks;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
